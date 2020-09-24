1af0adf1c398ee500e846272d4d01d8ad9084d5d
Apuroop1 <apuroop1reddy@gmail.com>
2020-09-24 17:41:43 -0500

    Add files via upload

---
+++ ICP5/ToDoICP/src/app/app.component.ts
@@ -0,0 +1,177 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // define list of items
+  items = ['check','add'];
+
+  // Write code to push new item
+
+  submitNewItem(s) {
+    this.items.push(s);
+  }
+
+  // Write code to complete item
+  completeItem() {
+
+  }
+
+  // Write code to delete item
+  deleteItem(todo) {
+    for ( let i = 0 ; i <= this.items.length ;i++){
+      if (todo === this.items[i]){
+        this.items.splice(i,1);
+      }
+    }
+  }
+
+  title = 'CalculatorICP';
+  ischeck = false;
+  elementsArray = [];
+  equation = '';
+  number = '';
+  clear();
+  clear() {
+    this.equation = '';
+    this.number = '';
+    this.elementsArray = [];
+  }
+
+  clearNumber() {
+    this.number = '';
+    return '';
+  }
+
+  equationAdd(a, b) {
+    this.elementsArray.push(a);
+    this.elementsArray.push(b);
+    this.equation += a;
+    this.equation += b;
+  }
+
+  multiply(a, b) {
+    let c = +this.elementsArray[a] * +this.elementsArray[b];
+    this.elementsArray[a] = c.toString();
+    this.elementsArray.splice(a + 1, 2);
+  }
+
+  divide(a, b) {
+    let c = +this.elementsArray[a] / +this.elementsArray[b];
+    this.elementsArray[a] = c.toString();
+    this.elementsArray.splice(a + 1, 2);
+  }
+
+  modulo(a, b) {
+    let c = +this.elementsArray[a] % +this.elementsArray[b];
+    this.elementsArray[a] = c.toString();
+    this.elementsArray.splice(a + 1, 2);
+  }
+
+  add(a, b) {
+    let c = +this.elementsArray[a] + +this.elementsArray[b];
+    this.elementsArray[a] = c.toString();
+    this.elementsArray.splice(a + 1, 2);
+  }
+
+  substract(a, b) {
+    let c = +this.elementsArray[a] - +this.elementsArray[b];
+    this.elementsArray[a] = c.toString();
+    this.elementsArray.splice(a + 1, 2);
+  }
+
+
+  calculate() {
+    for (let i = 0; i < this.elementsArray.length; i++) {
+      if (this.elementsArray[i] === '*') {
+        this.multiply(i - 1, i + 1);
+        i = i - 2;
+      } else if (this.elementsArray[i] === '/') {
+        this.divide(i - 1, i + 1);
+        i = i - 2;
+      } else if (this.elementsArray[i] === '%') {
+        this.modulo(i - 1, i + 1);
+        i = i - 2;
+      }
+    }
+    for (let i = 0; i < this.elementsArray.length; i++) {
+      if (this.elementsArray[i] === '+') {
+        this.add(i - 1, i + 1);
+        i = i - 2;
+      } else if (this.elementsArray[i] === '-') {
+        this.substract(i - 1, i + 1);
+        i = i - 2;
+      }
+    }
+  }
+  butcick(str) {
+    console.log('check');
+    if(this.ischeck){
+      this.clear();
+    }
+    this.ischeck = false;
+    switch (str) {
+      case '+':
+      {
+        this.equationAdd(this.number, '+');
+        this.clearNumber();
+        break;
+      }
+      case '-':
+      {
+        this.equationAdd(this.number, '-');
+        this.clearNumber();
+        break;
+      }
+
+      case '*':
+      {
+        this.equationAdd(this.number, '*');
+        this.clearNumber();
+        break;
+      }
+      case '/':
+      {
+        this.equationAdd(this.number, '/');
+        this.clearNumber();
+        break;
+      }
+
+      case '%':
+      {
+        this.equationAdd(this.number, '%');
+        this.clearNumber();
+        break;
+      }
+      case 'Clear':
+      {
+        this.clear();
+        break;
+
+      }
+
+      case '=':
+      {
+        this.ischeck = true;
+        this.equationAdd(this.number, '=');
+        this.elementsArray.pop();
+        this.calculate();
+        this.number = this.elementsArray[0];
+        this.equation = '';
+        this.elementsArray.pop();
+        break;
+      }
+
+      default:
+      {
+        this.number += str;
+        break;
+      }
+
+    }
+  }
+}
